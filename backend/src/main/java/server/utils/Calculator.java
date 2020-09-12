0e140cde8a00a8851d4f5ee20ae887bbf7b86391
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-09-12 20:21:25 -0300

    Implement factorial calculation

--- backend/src/main/java/server/utils/Calculator.java
+++ backend/src/main/java/server/utils/Calculator.java
@@ -3,7 +3,10 @@ package server;
 public class Calculator {
 
   public static int factorial(int number) {
-    return 1;
+    if (number == 0)    
+      return 1;    
+    else    
+      return(number * factorial(number-1));    
   }
-  
+
 }
