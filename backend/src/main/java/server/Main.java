064087f317219e961d00d657b24a4e2026e5e3d4
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-09-12 12:10:23 -0300

    Setup docker

--- backend/src/main/java/server/Main.java
+++ backend/src/main/java/server/Main.java
@@ -1,13 +1,21 @@
 package server;
 
-import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class Main {
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
 
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
   }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+
 }
