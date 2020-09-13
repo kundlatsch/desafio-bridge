fe6a1e6a345c0004d869f91bd2bf7b77e2e8a070
kundlatsch <gustavo.kundlatsch@gmail.com>
2020-09-13 12:08:21 -0300

    Create factorial calculation screen

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/factorial-api';
 
 import './styles.css';
-// TODO: create and import logo
-//<img src={logo} alt="Factorial!"/>
 
 const Home = () => {
+
+  const [factorialNumber, setFactorialNumber] = useState('');
+  const [result, setResult] = useState('Teste');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const number = parseInt(factorialNumber);
+    if (isNaN(number)) {
+      setResult('Insira um número válido');
+    }
+    else {
+      const response = await api.post('/factorial', { number });
+      const { numberFactorial } = response.data;
+      setResult(numberFactorial);
+    }
+
+  }
+
   return (
     <div id="page-home">
       <div className="content">
@@ -13,9 +31,24 @@ const Home = () => {
         </header>
 
         <main>
-          <h1>
+          <h2>
             Insira um número para calcular seu fatorial
-          </h1>
+          </h2>
+
+          <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="number"
+                id="numer"
+                onChange={event => setFactorialNumber(event.target.value)}
+              />
+
+            <button type="submit">
+              Calcular
+            </button> 
+          </form>
+
+          <p>{result}</p>
 
           <Link to="/history">
             <strong>
